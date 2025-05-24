5150290b4133ea8ce26773d409f155c9f188e60e
Anderson Rezende <andersonrezende17@hotmail.com>
2025-05-24 00:51:29 -0300

    Introduce user memory repository

---
+++ internal/adapters/repository/user_memory_repository.go
@@ -0,0 +1,54 @@
+package repository
+
+import (
+	"errors"
+	"gohexarc/internal/domain"
+)
+
+type InMemoryUserRepository struct {
+	users map[string]domain.User
+}
+
+func NewInMemoryUserRepository() *InMemoryUserRepository {
+	return &InMemoryUserRepository{users: make(map[string]domain.User)}
+}
+
+func (r *InMemoryUserRepository) Create(user domain.User) error {
+	if _, exists := r.users[user.ID]; exists {
+		return errors.New("user already exists")
+	}
+	r.users[user.ID] = user
+	return nil
+}
+
+func (r *InMemoryUserRepository) GetById(id string) (domain.User, error) {
+	user, ok := r.users[id]
+	if !ok {
+		return domain.User{}, errors.New("user not found")
+	}
+	return user, nil
+}
+
+func (r *InMemoryUserRepository) Update(user domain.User) error {
+	if _, exists := r.users[user.ID]; !exists {
+		return errors.New("user not found")
+	}
+	r.users[user.ID] = user
+	return nil
+}
+
+func (r *InMemoryUserRepository) Delete(id string) error {
+	if _, exists := r.users[id]; !exists {
+		return errors.New("user not found")
+	}
+	delete(r.users, id)
+	return nil
+}
+
+func (r *InMemoryUserRepository) List() ([]domain.User, error) {
+	users := make([]domain.User, 0, len(r.users))
+	for _, user := range r.users {
+		users = append(users, user)
+	}
+	return users, nil
+}
